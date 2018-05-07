416a1a69f6169058c2ae98576fa0fbe1d349cd1e
atalw <akshitalwar11@gmail.com>
2018-05-07 11:04:54 +0530

    Cleaned up js and css

--- js/main.js
+++ js/main.js
@@ -1,105 +1,14 @@
 (function ($) {
 
     "use strict";
-    $(".carousel-inner .item:first-child").addClass("active");
+    // $(".carousel-inner .item:first-child").addClass("active");
     /* Mobile menu click then remove
     ==========================*/
     $(".mainmenu-area #mainmenu li a").on("click", function () {
         $(".navbar-collapse").removeClass("in");
     });
-    /*WoW js Active
-    =================*/
-    new WOW().init({
-        mobile: true,
-    });
-    /* Scroll to top
-    ===================*/
-    $.scrollUp({
-        scrollText: '<i class="fa fa-angle-up"></i>',
-        easingType: 'linear',
-        scrollSpeed: 900,
-        animation: 'fade'
-    });
-    /*--------------------
-       MAGNIFIC POPUP JS
-       ----------------------*/
-    var magnifPopup = function () {
-        $('.work-popup').magnificPopup({
-            type: 'image',
-            removalDelay: 300,
-            mainClass: 'mfp-with-zoom',
-            gallery: {
-                enabled: true
-            },
-            zoom: {
-                enabled: true, // By default it's false, so don't forget to enable it
-
-                duration: 300, // duration of the effect, in milliseconds
-                easing: 'ease-in-out', // CSS transition easing function
-
-                // The "opener" function should return the element from which popup will be zoomed in
-                // and to which popup will be scaled down
-                // By defailt it looks for an image tag:
-                opener: function (openerElement) {
-                    // openerElement is the element on which popup was initialized, in this case its <a> tag
-                    // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-                    return openerElement.is('img') ? openerElement : openerElement.find('img');
-                }
-            }
-        });
-    };
-    // Call the functions
-    magnifPopup();
-
-    //Background Parallax
-    $('.header-area').parallax("50%", -0.4);
-    $('.price-area').parallax("50%", -0.5);
-    $('.testimonial-area').parallax("10%", -0.2);
-
 
     $('#accordion .panel-title a').prepend('<span></span>');
-
-
-
-
-
-
-    //Function to animate slider captions
-    function doAnimations(elems) {
-        //Cache the animationend event in a variable
-        var animEndEv = 'webkitAnimationEnd animationend';
-
-        elems.each(function () {
-            var $this = $(this),
-                $animationType = $this.data('animation');
-            $this.addClass($animationType).one(animEndEv, function () {
-                $this.removeClass($animationType);
-            });
-        });
-    }
-
-    //Variables on page load
-    var $myCarousel = $('.caption-slider'),
-        $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
-
-    //Initialize carousel
-    $myCarousel.carousel();
-
-    //Animate captions in first slide on page load
-    doAnimations($firstAnimatingElems);
-
-    //Pause carousel
-    $myCarousel.carousel('pause');
-
-
-    //Other slides to be animated on carousel slide event
-    $myCarousel.on('slide.bs.carousel', function (e) {
-        var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
-        doAnimations($animatingElems);
-    });
-
-
-
 
 
     // Select all links with hashes
@@ -137,11 +46,6 @@
                 }
             }
         });
-
-
-
-
-
 
     /* Preloader Js
     ===================*/
